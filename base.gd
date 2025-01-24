e44e5b62f270ab32162707dd932a68b185ced0d0
Jacob Richardson <Jake.Richardson@niceincontact.com>
2025-01-24 00:59:55 -0700

    Finished up basic menu and draw deck

--- base.gd
+++ base.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
-@onready var menu_layer = $"menu_layer"
+@onready var canvas_layer = $"menu_layer"
 @onready var map = $"map"
 @onready var music = $"music"
 const cards_draw_path = "res://cards_draw"
 const cards_ships_path = "res://cards_ships"
-@onready var menu_empty = preload("res://menu.tscn")
+@onready var menu = preload("res://menu.tscn")
+@onready var settings = preload("res://settings.tscn")
+@onready var draw_deck = []
 @onready var ships_all = []
 @onready var player_1_deck = []
 @onready var player_2_deck = []
 @onready var deck_races = ["Pirate", "Alien"]
 @onready var player_two_choose = false
 @onready var player_two_setup = false
-var menu_empty_instance
+var menu_instance
+var settings_instance
 
 func _ready():
 	# Starts music
@@ -21,8 +24,26 @@ func _ready():
 	map.position.x += 1278
 	map.position.y += 720
 	# Creates initial menu
-	menu_empty_instance = menu_empty.instantiate()
-	menu_layer.add_child(menu_empty_instance)
+	menu_instance = menu.instantiate()
+	canvas_layer.add_child(menu_instance)
+	# Creates settings menu
+	settings_instance = settings.instantiate()
+	settings_instance.hide()
+	canvas_layer.add_child(settings_instance)
+	# Creates settings menu listeners
+	settings_instance.get_node("music").pressed.connect(_on_music_pressed)
+	settings_instance.get_node("quit").pressed.connect(_on_quit_pressed)
+	# Initializes and places all draw cards in deck
+	var dir_deck = DirAccess.open(cards_draw_path)
+	dir_deck.list_dir_begin()
+	var file_name_deck = dir_deck.get_next()
+	while file_name_deck != "":
+		if file_name_deck.ends_with(".tscn") or file_name_deck.ends_with(".scn"):
+			var scene_path = cards_draw_path + "/" + file_name_deck
+			var packed_scene = load(scene_path)
+			var instance = packed_scene.instantiate()
+			draw_deck.push_back(instance)
+		file_name_deck = dir_deck.get_next()
 	# Initializes and places all ships in an array
 	var dir = DirAccess.open(cards_ships_path)
 	dir.list_dir_begin()
@@ -41,6 +62,8 @@ func _ready():
 	$"alien_arrow".position.y += 1030
 	$"pirate_carrier_5".position.x += 1280
 	$"pirate_carrier_5".position.y += 410
+	$"d1_1".position.x += 1820
+	$"d1_1".position.y += 720
 
 func _on_button_pressed(button):
 	# Adds all ships to players deck
@@ -62,15 +85,26 @@ func _on_button_pressed(button):
 		if deck_races[x] == button.text:
 			deck_races.remove_at(x)
 	# Removes all buttons
-	for x in menu_empty_instance.get_children():
+	for x in menu_instance.get_children():
 		x.queue_free()
+
+func _on_music_pressed():
+	music.playing = !music.playing
+	
+func _on_quit_pressed():
+	get_tree().quit()
 
 func _input(event):
 	if event.is_action_pressed("escape"):
-		get_tree().quit()
+		if menu_instance.get_child_count() == 0:
+			if settings_instance.visible:
+				settings_instance.hide()
+			else:
+				settings_instance.show()
 
 func _physics_process(_delta):
 	if player_two_choose == true and player_two_setup == false:
+		# Creates menu options to select from for player 2
 		create_menu_options()
 		player_two_setup = true
 	map_movement_manager()
@@ -79,8 +113,10 @@ func create_menu_options():
 	for x in deck_races:
 		var button = Button.new()
 		button.text = x
+		button.custom_minimum_size = Vector2(350, 120)
+		button.add_theme_font_size_override("font_size", 75)
 		button.connect("pressed", Callable(self, "_on_button_pressed").bind(button))
-		menu_empty_instance.add_child(button)
+		menu_instance.add_child(button)
 
 func map_movement_manager():
 	var mouse_position = get_viewport().get_mouse_position()
